2fe4d40041844650448284c07f89f6a9c7c0a12f
zhangdaiscott <zhangdaiscott@163.com>
2026-08-24 11:41:17 +0800

    删除多余升级SQL

--- jeecg-boot/jeecg-module-system/jeecg-system-start/src/main/resources/flyway/sql/mysql/V3.9.5_1__jimureport_upgrade.sql
+++
@@ -1,8 +0,0 @@
--- ---date:20260625-----for: 字典索引修改
-ALTER TABLE `jimu_dict`
-DROP INDEX `uk_sd_dict_code`,
-ADD INDEX `idx_jd_dict_code`(`dict_code`) USING BTREE,
-ADD INDEX `idx_jd_tenant_id`(`tenant_id`) USING BTREE,
-ADD INDEX `idx_jd_create_by`(`create_by`) USING BTREE,
-ADD INDEX `idx_jd_dict_code_tenant_id`(`dict_code`, `tenant_id`) USING BTREE,
-ADD INDEX `idx_jd_dict_code_create_by`(`dict_code`, `create_by`) USING BTREE;
